261d3717f5cf3fc735a7a978e8cc801e6ea64d12
Jeremiah Desktop <jereberries1215@gmail.com>
2022-12-30 20:25:57 -0800

    Added image

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+const reveal = () => {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
-}
+  }
+};
+
+
   
 window.addEventListener("scroll", reveal);
 
